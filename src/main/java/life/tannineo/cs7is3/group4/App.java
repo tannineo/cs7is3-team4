f7ce99a4f03ff44160723834069d7dfaac31a78a
tannineo <tannineo@outlook.com>
2020-04-01 19:32:40 +0100

    add query parsing

--- src/main/java/life/tannineo/cs7is3/group4/App.java
+++ src/main/java/life/tannineo/cs7is3/group4/App.java
@@ -1,6 +1,10 @@
 package life.tannineo.cs7is3.group4;
 
+import life.tannineo.cs7is3.group4.entity.DocumentQuery;
+import life.tannineo.cs7is3.group4.parser.QueryParser;
+
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class App {
 
@@ -16,7 +20,13 @@ public class App {
         // endregion
 
         // region 3. parse queries
-
+        QueryParser queryParser = new QueryParser();
+        ArrayList<DocumentQuery> documentQueries = queryParser.readQueries("topics");
+//        for (DocumentQuery documentQuery : documentQueries) {
+//            System.out.println(documentQuery.Title);
+//            System.out.println(documentQuery.description);
+//            System.out.println(documentQuery.Narrative);
+//        }
         // endregion
 
         // region 4. search
---
+++ src/main/java/life/tannineo/cs7is3/group4/parser/QueryParser.java
@@ -0,0 +1,39 @@
+package life.tannineo.cs7is3.group4.parser;
+
+import life.tannineo.cs7is3.group4.entity.DocumentQuery;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class QueryParser {
+    public ArrayList<DocumentQuery> readQueries(String queryFilePath) {
+        ArrayList<DocumentQuery> QueryArray = new ArrayList<>();
+        try {
+            File file = new File(Paths.get(queryFilePath).toString());
+            Document doc = Jsoup.parse(file, "UTF-8");
+            Elements elements = doc.getElementsByTag("top");
+            for (Element el : elements) {
+                DocumentQuery documentQuery = new DocumentQuery();
+                Elements QueryId = el.getElementsByTag("num");
+                Elements Title = el.getElementsByTag("title");
+                Elements Desc = el.getElementsByTag("desc");
+                Elements Narrative = el.getElementsByTag("narr");
+
+                documentQuery.queryId = QueryId.text().split(" ")[1];
+                documentQuery.Title = Title.text().replaceAll("[^a-zA-Z ]", "").toLowerCase();
+                documentQuery.description = Desc.text().split("Narrative")[0].replace("Description:", "").replaceAll("[^a-zA-Z ]", "").toLowerCase();
+                documentQuery.Narrative = Narrative.text().replace("Narrative:", "").replaceAll("[^a-zA-Z ]", "").toLowerCase();
+                QueryArray.add(documentQuery);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return QueryArray;
+    }
+}
