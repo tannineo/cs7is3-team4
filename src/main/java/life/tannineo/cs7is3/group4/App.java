bc46e4e11580c5efb574a7b957859e7fca9d7192
Vishal <vishalkumartcd@gmail.com>
2020-04-16 00:42:21 +0100

    Separating Query parse and search method
    Separating Query parse and search method to make code modular.

--- src/main/java/life/tannineo/cs7is3/group4/App.java
+++ src/main/java/life/tannineo/cs7is3/group4/App.java
@@ -9,6 +9,7 @@ import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
@@ -109,116 +110,124 @@ public class App {
             }
 
             // endregion
-
+        }
             // region 3. query parsing
-            ArrayList<DocumentQuery> documentQueries = new QueryParser().readQueries("topics");
-//        for (DocumentQuery documentQuery : documentQueries) {
-//            System.out.println(documentQuery.title);
-//            System.out.println(documentQuery.description);
-//            System.out.println(documentQuery.narrative);
-//        }
             LinkedHashMap<String, Query> queryLinkedHashMap = new LinkedHashMap<>();
-
-            // parameter tuning
-            HashMap<String, Float> boosts = new HashMap<>();
-
-            // search_result_1586350867320 MAP = 0.2993
-//        boosts.put(FieldName.TEXT.getName(), 20f);
-//        boosts.put(FieldName.META.getName(), 5f);
-//        boosts.put(FieldName.GRAPHIC.getName(), 3f);
-//        boosts.put(FieldName.HEADLINE.getName(), 2f);
-//        boosts.put(FieldName.HEADER.getName(), 2f);
-
-            // search_result_1586351017203 MAP = 0.2870
-//        boosts.put(FieldName.TEXT.getName(), 20f);
-//        boosts.put(FieldName.META.getName(), 5f);
-//        boosts.put(FieldName.GRAPHIC.getName(), 10f);
-//        boosts.put(FieldName.HEADLINE.getName(), 5f);
-//        boosts.put(FieldName.HEADER.getName(), 5f);
-//        boosts.put(FieldName.HT.getName(), 5f);
-//        boosts.put(FieldName.TYPE.getName(), 5f);
-//        boosts.put(FieldName.SUBJECT.getName(), 5f);
-//        boosts.put(FieldName.DATE.getName(), 5f);
-
-            // search_result_1586351149955 MAP = 0.2884
-//        boosts.put(FieldName.TEXT.getName(), 20f);
-//        boosts.put(FieldName.META.getName(), 5f);
-//        boosts.put(FieldName.GRAPHIC.getName(), 10f);
-//        boosts.put(FieldName.TYPE.getName(), 5f);
-//        boosts.put(FieldName.SUBJECT.getName(), 5f);
-//        boosts.put(FieldName.DATE.getName(), 5f);
-
-            // search_result_1586351229036 MAP = 0.2883
-//        boosts.put(FieldName.TEXT.getName(), 20f);
-//        boosts.put(FieldName.META.getName(), 10f);
-//        boosts.put(FieldName.GRAPHIC.getName(), 5f);
-
-            // search_result_1586351305185 MAP = 0.3010
-//        boosts.put(FieldName.TEXT.getName(), 30f);
-//        boosts.put(FieldName.META.getName(), 5f);
-//        boosts.put(FieldName.GRAPHIC.getName(), 3f);
-//        boosts.put(FieldName.HEADLINE.getName(), 2f);
-//        boosts.put(FieldName.HEADER.getName(), 2f);
-
-            // removed k-stem and snowball using these MAP = 0.3010
-            boosts.put(FieldName.TEXT.getName(), 40f);
-            boosts.put(FieldName.GRAPHIC.getName(), 3f);
-            boosts.put(FieldName.HEADLINE.getName(), 2f);
-            boosts.put(FieldName.HEADER.getName(), 2f);
-            boosts.put(FieldName.HT.getName(), 5f);
-
-
-//            MultiFieldQueryParser multiFieldQueryParser = new MultiFieldQueryParser(FieldName.getAllNamesExceptNonSense(), mySynonymAnalyzer, boosts);
-            MultiFieldQueryParser multiFieldQueryParser = new MultiFieldQueryParser(FieldName.getAllNamesExceptNonSense(), mCustomAnalyzer_Syn_stp, boosts); /* use mCustomAnalyzer_Syn_stp */
-            // multiFieldQueryParser.setAllowLeadingWildcard(true);
-            for (DocumentQuery dq : documentQueries) {
-                System.out.println("Parsing Query ID:" + dq.queryId);
-//            String parsedQueryStr = dq.title + " " + dq.description;
-                String parsedQueryStr = dq.title + " " + dq.description + " " + dq.narrative;
-//            String parsedQueryStr = MyQueryStringParser.parseQueryString(dq.title + " " + dq.description + " " + dq.narrative);
-                Query qry = multiFieldQueryParser.parse(parsedQueryStr);
-                System.out.println(qry.toString());
-                queryLinkedHashMap.put(dq.queryId, qry);
-            }
-
-            System.out.println("Parsed all queries... " + queryLinkedHashMap.entrySet().size() + " in total!");
+        	parse_query(mCustomAnalyzer_Syn_stp, queryLinkedHashMap);
             // endregion
+        	// region 4. query searching
+            search_query(bm25Similarity,queryLinkedHashMap);
 
-            // region 4. search
-            Directory dirr = FSDirectory.open(Paths.get(App.INDEX_PATH));
-            DirectoryReader dirReader = DirectoryReader.open(dirr);
-            IndexSearcher searcher = new IndexSearcher(dirReader);
-            searcher.setSimilarity(bm25Similarity);
-
-            ArrayList<String> resultArr = new ArrayList<>();
-
-            for (Map.Entry<String, Query> queryEntry : queryLinkedHashMap.entrySet()) {
-                TopDocs topDocs = searcher.search(queryEntry.getValue(), HITS_PER_PAGE);
-                ScoreDoc[] topHits = topDocs.scoreDocs;
-
-                for (ScoreDoc hit : topHits) {
-                    Document doc = searcher.doc(hit.doc);
-                    String result = queryEntry.getKey() + " 0 " + doc.get(FieldName.DOCNO.getName()) + " 0 " + hit.score + " STANDARD";
-//                System.out.println(result);
-                    // add the result
-                    resultArr.add(result);
-                }
-            }
-            // endregion
-
-            // region 5. gen results
-            String filename = "search_result_" + new Date().getTime();
-            Path resultPath = Paths.get(filename);
-            Files.write(resultPath, resultArr, StandardCharsets.UTF_8);
-
-            System.out.println(filename + " complete!");
-            // endregion
-            
             long end = System.currentTimeMillis();
 
             NumberFormat secFormatter = new DecimalFormat("#0.00000");
             System.out.println("Program Running time is " + secFormatter.format((end - start) / 1000d) + " seconds");
 
+        
+    }
+    
+    private static void parse_query(Analyzer mCustomAnalyzer_Syn_stp, LinkedHashMap<String, Query> queryLinkedHashMap) throws ParseException {
+        ArrayList<DocumentQuery> documentQueries = new QueryParser().readQueries("topics");
+//    for (DocumentQuery documentQuery : documentQueries) {
+//        System.out.println(documentQuery.title);
+//        System.out.println(documentQuery.description);
+//        System.out.println(documentQuery.narrative);
+//    }
+
+        // parameter tuning
+        HashMap<String, Float> boosts = new HashMap<>();
+
+        // search_result_1586350867320 MAP = 0.2993
+//    boosts.put(FieldName.TEXT.getName(), 20f);
+//    boosts.put(FieldName.META.getName(), 5f);
+//    boosts.put(FieldName.GRAPHIC.getName(), 3f);
+//    boosts.put(FieldName.HEADLINE.getName(), 2f);
+//    boosts.put(FieldName.HEADER.getName(), 2f);
+
+        // search_result_1586351017203 MAP = 0.2870
+//    boosts.put(FieldName.TEXT.getName(), 20f);
+//    boosts.put(FieldName.META.getName(), 5f);
+//    boosts.put(FieldName.GRAPHIC.getName(), 10f);
+//    boosts.put(FieldName.HEADLINE.getName(), 5f);
+//    boosts.put(FieldName.HEADER.getName(), 5f);
+//    boosts.put(FieldName.HT.getName(), 5f);
+//    boosts.put(FieldName.TYPE.getName(), 5f);
+//    boosts.put(FieldName.SUBJECT.getName(), 5f);
+//    boosts.put(FieldName.DATE.getName(), 5f);
+
+        // search_result_1586351149955 MAP = 0.2884
+//    boosts.put(FieldName.TEXT.getName(), 20f);
+//    boosts.put(FieldName.META.getName(), 5f);
+//    boosts.put(FieldName.GRAPHIC.getName(), 10f);
+//    boosts.put(FieldName.TYPE.getName(), 5f);
+//    boosts.put(FieldName.SUBJECT.getName(), 5f);
+//    boosts.put(FieldName.DATE.getName(), 5f);
+
+        // search_result_1586351229036 MAP = 0.2883
+//    boosts.put(FieldName.TEXT.getName(), 20f);
+//    boosts.put(FieldName.META.getName(), 10f);
+//    boosts.put(FieldName.GRAPHIC.getName(), 5f);
+
+        // search_result_1586351305185 MAP = 0.3010
+//    boosts.put(FieldName.TEXT.getName(), 30f);
+//    boosts.put(FieldName.META.getName(), 5f);
+//    boosts.put(FieldName.GRAPHIC.getName(), 3f);
+//    boosts.put(FieldName.HEADLINE.getName(), 2f);
+//    boosts.put(FieldName.HEADER.getName(), 2f);
+
+        // removed k-stem and snowball using these MAP = 0.3010
+        boosts.put(FieldName.TEXT.getName(), 40f);
+        boosts.put(FieldName.GRAPHIC.getName(), 3f);
+        boosts.put(FieldName.HEADLINE.getName(), 2f);
+        boosts.put(FieldName.HEADER.getName(), 2f);
+        boosts.put(FieldName.HT.getName(), 5f);
+
+
+//        MultiFieldQueryParser multiFieldQueryParser = new MultiFieldQueryParser(FieldName.getAllNamesExceptNonSense(), mySynonymAnalyzer, boosts);
+        MultiFieldQueryParser multiFieldQueryParser = new MultiFieldQueryParser(FieldName.getAllNamesExceptNonSense(), mCustomAnalyzer_Syn_stp, boosts); /* use mCustomAnalyzer_Syn_stp */
+        // multiFieldQueryParser.setAllowLeadingWildcard(true);
+        for (DocumentQuery dq : documentQueries) {
+            System.out.println("Parsing Query ID:" + dq.queryId);
+//        String parsedQueryStr = dq.title + " " + dq.description;
+            String parsedQueryStr = dq.title + " " + dq.description + " " + dq.narrative;
+//        String parsedQueryStr = MyQueryStringParser.parseQueryString(dq.title + " " + dq.description + " " + dq.narrative);
+            Query qry = multiFieldQueryParser.parse(parsedQueryStr);
+            System.out.println(qry.toString());
+            queryLinkedHashMap.put(dq.queryId, qry);
         }
+
+        System.out.println("Parsed all queries... " + queryLinkedHashMap.entrySet().size() + " in total!");
+        // endregion
+
+    }
+    private static void search_query(BM25Similarity bm25Similarity, LinkedHashMap<String, Query> queryLinkedHashMap) throws IOException {
+        Directory dirr = FSDirectory.open(Paths.get(App.INDEX_PATH));
+        DirectoryReader dirReader = DirectoryReader.open(dirr);
+        IndexSearcher searcher = new IndexSearcher(dirReader);
+        searcher.setSimilarity(bm25Similarity);
+
+        ArrayList<String> resultArr = new ArrayList<>();
+
+        for (Map.Entry<String, Query> queryEntry : queryLinkedHashMap.entrySet()) {
+            TopDocs topDocs = searcher.search(queryEntry.getValue(), HITS_PER_PAGE);
+            ScoreDoc[] topHits = topDocs.scoreDocs;
+
+            for (ScoreDoc hit : topHits) {
+                Document doc = searcher.doc(hit.doc);
+                String result = queryEntry.getKey() + " 0 " + doc.get(FieldName.DOCNO.getName()) + " 0 " + hit.score + " STANDARD";
+//            System.out.println(result);
+                // add the result
+                resultArr.add(result);
+            }
+        }
+        // endregion
+
+        // region 5. gen results
+        String filename = "search_result_" + new Date().getTime();
+        Path resultPath = Paths.get(filename);
+        Files.write(resultPath, resultArr, StandardCharsets.UTF_8);
+
+        System.out.println(filename + " complete!");
+        // endregion
     }
 }
